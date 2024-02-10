6290e7d53a09836dc900ede377e0165face0365b
ankitakanoji10 <95422118+ankitakanoji10@users.noreply.github.com>
2024-02-10 16:48:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { default: slugify } = require('slugify');
 dotenv.config();
 connectDB();
 app.use(cors({
-  origin: "https://unisync-d3hz.onrender.com",
+  origin: "https://unisync-front.vercel.app",
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
   optionsSuccessStatus: 204 
@@ -118,7 +118,7 @@ app.get('/login',
       function(req, res) {
         // Successful authentication, redirect home.
         console.log(req.user);
-        res.redirect('https://unisync-d3hz.onrender.com/profile-page');
+        res.redirect('https://unisync-front.vercel.app/profile-page');
 });
 // // Login Route
 // app.get('/login', 
